09cda54655edcc59f988f83ce626fe92a5c2566e
Lusine1509 <andriasyanlusine@gmail.com>
2021-08-18 14:16:35 +0400

    2nd task update

--- index.js
+++ index.js
@@ -1,52 +1,53 @@
-const fs = require('fs')
+const fs = require('fs');
+const path= require('path')
 
 fs.readFile('text.txt', function (err, data) {
     if (err) throw err;
     const array = data.toString().split("\n");
     const newArr = [];
     let result = {};
-    let obj7={}
- 
+    let final = {}
+
+
+    const merge = (...arguments) => {
+        let target = {};
+        const merger = (obj) => {
+            for (let prop in obj) {
+                if (obj.hasOwnProperty(prop)) {
+                    if (Object.prototype.toString.call(obj[prop]) === '[object Object]') {
+                        target[prop] = merge(target[prop], obj[prop]);
+                    } else {
+                        target[prop] = obj[prop];
+                    }
+                }
+            }
+        };
+
+
+        for (let i = 0; i < arguments.length; i++) {
+            merger(arguments[i]);
+        }
+
+        return target;
+    };
+
 
     for (let i = 0; i < array.length; i++) {
-       
+
         const obj = {};
         newArr.push(array[i].split('.').reverse())
         obj[newArr[i][0].split('=')[0]] = newArr[i][0].split('=')[1]
 
 
         newArr[i].shift();
-        
+
         result = newArr[i].reduce((res, key) => ({ [key]: res }), obj)
-        Object.assign(obj7,result)
-        console.log(result);
-      
-       
+
+        final = merge(final, result)
+
     }
-    
-    
-    //     let arr = data.toString().split('\n');
-    //     let obj = {};
-    //     let nestedObj= {};
-    //     let key;
-    //     let bigKey;
-    //     let value;
-    //     arr.forEach((row) => {
-    //         if(row.includes('.')){
-    //             key = row.slice(row.lastIndexOf('.')+1, row.indexOf('='));
-    //             bigKey = row.slice(0, row.indexOf('.'))
-    //             value = row.slice(row.indexOf('=')+1);
-    //             nestedObj[key]=value;
-    //             obj[bigKey]=nestedObj;
-    //         }
-    //         else{
-    //             key = row.slice(0, row.indexOf('='));
-    //             value = row.slice(row.indexOf('=')+1);
-    //             obj[key] = value;
-    //         }
-    //     });
-    //   console.log(nestedObj);
-     }
+    fs.writeFileSync(path.join(__dirname, 'text.json'), JSON.stringify(final));
+}
 
 
 );
